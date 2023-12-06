9bef3df273f21427d74a4be176ca0dd93604179f
Gustavo <126978010+masterego95@users.noreply.github.com>
2023-12-06 20:45:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ function submitForm() {
     const courseDescription = document.getElementById('courseDescription').value;
     const courseImage = document.getElementById('courseImage').value;
 
-    // Você pode fazer o que quiser com os dados, como enviá-los para um servidor ou armazená-los localmente
+  
     console.log('Nome do Curso:', courseName);
     console.log('Professor Responsável:', courseProfessor);
     console.log('Categoria:', courseCategory);
     console.log('Descrição:', courseDescription);
     console.log('URL da Imagem:', courseImage);
 
-    // Você pode adicionar lógica adicional, como resetar o formulário ou redirecionar o usuário
-    // document.getElementById('courseForm').reset();
+  
 }
